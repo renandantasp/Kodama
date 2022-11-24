b905d8a45a404be205fe65b4e1f7018abe0f3a62
renandantasp <renandantasp@gmail.com>
2022-11-24 04:14:45 -0300

    fix and cleanup project configuration and remove example code

--- kodama_f/src/App.tsx
+++ kodama_f/src/App.tsx
@@ -2,15 +2,13 @@ import type { ReactElement } from 'react'
 import { lazy } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-const Gallery = lazy(async () => import('pages/Gallery'))
-const Details = lazy(async () => import('pages/Details'))
+const Homepage = lazy(async () => import('pages/Homepage'))
 
 export default function App(): ReactElement {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Gallery />} />
-				<Route path=':fruitName' element={<Details />} />
+				<Route path='/' element={<Homepage />} />
 			</Routes>
 		</BrowserRouter>
 	)
---
+++ kodama_f/src/pages/Homepage.tsx
@@ -0,0 +1,7 @@
+import type { ReactElement } from 'react'
+
+export default function Homepage(): ReactElement {
+	const a = 2
+
+	return <div>{a}</div>
+}
