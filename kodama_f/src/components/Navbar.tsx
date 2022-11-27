8b2e8035be7b2fae173d32e3284954783ae435ab
renandantasp <renandantasp@gmail.com>
2022-11-27 13:49:49 -0300

    feat(layout): finish responsive layout with placeholder links

--- kodama_f/src/components/Navbar.tsx
+++ kodama_f/src/components/Navbar.tsx
@@ -3,7 +3,11 @@
 import Modal from '@mui/material/Modal'
 import type { ReactElement } from 'react'
 import { useState } from 'react'
-import { AiOutlineEllipsis, AiOutlineMenu } from 'react-icons/ai'
+import {
+	AiOutlineClose,
+	AiOutlineEllipsis,
+	AiOutlineMenu
+} from 'react-icons/ai'
 
 export default function Navbar(): ReactElement {
 	const [open, setOpen] = useState(false)
@@ -12,7 +16,9 @@ export default function Navbar(): ReactElement {
 
 	return (
 		<div className='flex items-center justify-between p-4 lg:p-10 lg:pt-8'>
-			<div className='font-black'> K O D A M A </div>
+			<a href='/' className='cursor-pointer font-black'>
+				K O D A M A
+			</a>
 			<input
 				className='mx-6 rounded-full bg-neutral-700 px-4 py-1 transition placeholder:text-sm placeholder:text-neutral-400 hover:bg-white placeholder:hover:text-neutral-800 focus:bg-white focus:text-black lg:flex-1 lg:py-2'
 				placeholder='&#xF002;   Search for games'
@@ -40,25 +46,42 @@ export default function Navbar(): ReactElement {
 			<Modal
 				open={open}
 				onClose={handleClose}
-				className='flex-end mt-14 mr-4 flex items-start justify-end'
+				className='flex-end mr-4 mt-4 flex items-start justify-end'
 				aria-labelledby='modal-modal-title'
 				aria-describedby='modal-modal-description'
 			>
-				<div className='flex h-3/4 flex-col'>
-					<div className='rounded-t-lg bg-purple-500 p-4'>
-						<a href='/' className='rounded-full bg-neutral-900 text-white'>
-							Home
+				<div className='flex w-64 flex-col duration-700 ease-in-out'>
+					<div className='flex flex-row items-center justify-between rounded-t-lg bg-neutral-900 p-4'>
+						<a href='/' className='mr-2 text-sm text-white hover:underline'>
+							Log In
 						</a>
-						<a href='/' className='rounded-full bg-neutral-900 text-white'>
-							Home
+						<a href='/' className='mr-2 text-sm text-white hover:underline'>
+							Sign Up
 						</a>
-						<a href='/' className='rounded-full bg-neutral-900 text-white'>
-							Home
-						</a>
+						<button onClick={handleClose} className='text-white'>
+							<AiOutlineClose />
+						</button>
 					</div>
-					<div className='w-32 rounded-b-lg bg-white p-4 text-neutral-900'>
-						<a href='/' className='text-lg font-medium'>
+					<div className='flex flex-col rounded-b-lg bg-white p-4 text-neutral-900'>
+						<a href='/' className='mb-2 text-lg font-medium'>
 							Home
+						</a>
+						<div className='mb-2 flex flex-col'>
+							<a href='/' className='text-lg font-medium'>
+								Link father
+							</a>
+							<a href='/' className='ml-4'>
+								Link father
+							</a>
+							<a href='/' className='ml-4'>
+								Link father
+							</a>
+							<a href='/' className='ml-4'>
+								Link father
+							</a>
+						</div>
+						<a href='/' className='mb-2 text-lg font-medium'>
+							Another Link
 						</a>
 					</div>
 				</div>
--- kodama_f/src/components/SidebarSubLink.tsx
+++ kodama_f/src/components/SidebarSubLink.tsx
@@ -10,11 +10,14 @@ export default function SidebarSubLink({
 	text
 }: Properties): ReactElement {
 	return (
-		<div className='group mb-2 flex w-full flex-row items-center transition ease-in-out'>
-			<div className='duration-400 duration-400 mr-2 flex h-8 w-8 items-center justify-center rounded bg-neutral-800 transition ease-in-out group-hover:bg-white group-hover:text-neutral-900'>
+		<a
+			href='/'
+			className='group mb-2 flex w-full cursor-pointer flex-row items-center transition ease-in-out'
+		>
+			<div className='duration-400 mr-2 flex h-8 w-8 items-center justify-center rounded bg-neutral-800 transition duration-500 ease-in-out group-hover:bg-white group-hover:text-neutral-900'>
 				{icon}
 			</div>
 			<p className=''>{text}</p>
-		</div>
+		</a>
 	)
 }
