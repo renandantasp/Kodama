8db5769a4eac91ca31069720129e31d5a92622c6
renandantasp <renandantasp@gmail.com>
2022-12-17 15:58:37 -0300

    refactor(css): minor css fixes

--- kodama_f/src/components/SidebarSubLink.tsx
+++ kodama_f/src/components/SidebarSubLink.tsx
@@ -14,7 +14,7 @@ export default function SidebarSubLink({
 			href='/'
 			className='group mb-2 flex w-full cursor-pointer flex-row items-center transition ease-in-out'
 		>
-			<div className='duration-400 mr-2 flex h-8 w-8 items-center justify-center rounded bg-neutral-800 transition duration-500 ease-in-out group-hover:bg-white group-hover:text-neutral-900'>
+			<div className='duration-400 h-8 mr-2 flex w-8 items-center justify-center rounded bg-neutral-800 py-1 transition duration-500 ease-in-out group-hover:bg-white group-hover:text-neutral-900'>
 				{icon}
 			</div>
 			<p className=''>{text}</p>
--- kodama_f/src/components/gameList.tsx
+++ kodama_f/src/components/gameList.tsx
@@ -4,7 +4,7 @@ import GameCard from './gameCard'
 
 export default function GameList({ games }: IGames | undefined): ReactElement {
 	return (
-		<div className='mx-6 flex flex-wrap items-center justify-around lg:flex-row'>
+		<div className='mx-6 flex flex-wrap items-start justify-around lg:flex-row'>
 			{games.map(game => (
 				<GameCard data={game} />
 			))}
