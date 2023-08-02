5a7262bcf1f303c25944e75088717500ba5c183d
Renan <renandantasp@gmail.com>
2023-08-02 18:21:34 -0300

    minor responsive fixes at search

--- client/src/components/gameSearchItem.tsx
+++ client/src/components/gameSearchItem.tsx
@@ -8,14 +8,17 @@ interface Props {
 
 function GameSearchItem({ game }: Props): ReactElement {
 	return (
-		<div className='flex flex-row rounded p-2 hover:bg-white hover:text-black'>
+		<a
+			href={`/games/${game.slug}`}
+			className='flex flex-row rounded-b p-2 hover:bg-white hover:text-black'
+		>
 			<img
 				src={game.background_image}
 				alt={game.name}
 				className=' h-14 mr-4 w-10 rounded object-cover'
 			/>
 
-			<a href={`/games/${game.slug}`}>
+			<div>
 				<div className='text-[0.75em]'>
 					{game.platforms === null ? (
 						<div />
@@ -26,8 +29,8 @@ function GameSearchItem({ game }: Props): ReactElement {
 					)}
 				</div>
 				<p>{game.name}</p>
-			</a>
-		</div>
+			</div>
+		</a>
 	)
 }
 
--- client/src/components/search.tsx
+++ client/src/components/search.tsx
@@ -13,32 +13,38 @@ function Search(): ReactElement {
 
 	const { isLoading, data } = SearchGame(query)
 
-	if (!isLoading) {
-		console.log(data)
-	}
+	const searchMode = query.length > 0 ? 'lg:rounded-b-sm ' : 'lg:rounded-full'
 
 	return (
 		<div className='z-50 w-[60%] lg:w-[80%] lg:pl-16'>
 			<input
-				className='ml-0 mr-3 w-full rounded-full bg-neutral-500 px-4 py-1.5 transition duration-300 ease-in-out placeholder:text-neutral-100 hover:bg-white hover:text-black hover:opacity-100 placeholder:hover:text-neutral-800 focus:bg-white focus:text-black focus:opacity-100 focus:outline-none placeholder:focus:text-black lg:py-2'
+				className={`${searchMode} content:border-t ml-0 mr-3 w-full rounded-full bg-neutral-500 px-4 py-1.5 opacity-50 transition duration-300 
+                ease-in-out placeholder:text-neutral-100 hover:bg-black hover:text-white hover:opacity-100 placeholder:hover:text-neutral-500
+                focus:bg-black focus:text-white focus:opacity-100 focus:outline-none placeholder:focus:text-neutral-500 lg:py-2`}
 				placeholder='&#xF002;   Search for games'
 				value={query}
 				onChange={onQueryChange}
 			/>
 			{query.length > 0 ? (
-				<div className='absolute left-0 w-[100%] rounded bg-black lg:left-auto lg:w-[73.2%]'>
+				<div className='absolute left-0 w-[100%] rounded-b bg-black lg:left-auto lg:w-[73.25%]'>
 					{isLoading ? (
 						<div className='h-32 flex flex-row justify-center py-8'>
 							<Loading />
 						</div>
 					) : (
 						<div>
-							{data === undefined ? (
-								<p className='p-2 text-center'> No games found. </p>
+							{data !== undefined ? (
+								<div>
+									{data.results.length === 0 ? (
+										<p className='p-2 text-center lg:rounded-b-full'> No games found. </p>
+									) : (
+										data.results.map(game => (
+											<GameSearchItem key={game.name} game={game} />
+										))
+									)}
+								</div>
 							) : (
-								data.results.map(game => (
-									<GameSearchItem key={game.name} game={game} />
-								))
+								<div />
 							)}
 						</div>
 					)}
