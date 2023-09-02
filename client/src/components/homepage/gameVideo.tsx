0823a6f11b067f77e251905bd663c0dd04ef3a53
Renan <renandantasp@gmail.com>
2023-09-02 20:30:16 -0300

    add blank image to games without image

--- client/src/components/homepage/gameVideo.tsx
+++ client/src/components/homepage/gameVideo.tsx
@@ -6,9 +6,18 @@ interface Props {
 }
 
 export default function GameVideo({ game, img_game }: Props): ReactElement {
+	console.log(img_game)
 	return (
 		<div className='aspect-w-16 aspect-h-9'>
-			<img src={img_game} alt={game} className='rounded-t-lg object-cover' />
+			{img_game !== null ? (
+				<img src={img_game} alt={game} className='rounded-t-lg object-cover' />
+			) : (
+				<img
+					src='/notFound.jpeg'
+					alt={game}
+					className='rounded-t-lg object-cover'
+				/>
+			)}
 		</div>
 	)
 }
